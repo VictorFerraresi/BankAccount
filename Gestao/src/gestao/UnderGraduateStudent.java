d3a90fb15a05ed89cdb86d83dc8e1c9f4dd7fadb
Victor Ferraresi <victor_ferraresi@hotmail.com>
2015-05-21 14:22:15 -0300

    Iniciei a criação das exceptions.

--- Gestao/src/gestao/UnderGraduateStudent.java
+++ Gestao/src/gestao/UnderGraduateStudent.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gestao;
 
-/**
- *
- * @author 41419839
- */
 public class UnderGraduateStudent extends Student {
     private String major;
     private String minor;
